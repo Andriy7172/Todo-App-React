2a5c9d87bbf00573831c51b728c2199fd9511f0a
Andrii <andriyko.zoh5@gmail.com>
2019-05-08 20:58:15 +0300

    Commit on production

---
+++ docs/precache-manifest.8be799ee3c80525decde382f927be0c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad1bfb475d21f58a55266993633ffed4",
+    "url": "/static/media/10.ad1bfb47.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "23123a6c2a94936bdda8",
+    "url": "/static/js/main.eff63e7e.chunk.js"
+  },
+  {
+    "revision": "3dc7648dda181db480d9",
+    "url": "/static/js/2.6c3e4f80.chunk.js"
+  },
+  {
+    "revision": "23123a6c2a94936bdda8",
+    "url": "/static/css/main.dca9d1eb.chunk.css"
+  },
+  {
+    "revision": "048c8a098de4141d31eb9e97b4f0ffd2",
+    "url": "/index.html"
+  }
+];
